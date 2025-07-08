f732b0744f78e4097bd60de3e1b85c0c93bb4d88
Beryl <10091181+SebasCapo@users.noreply.github.com>
2025-07-07 23:07:17 -0300

    Experiments System (#2)
    * Push
    
    * IsEnabled
    
    * New Experiemnt + Refactor

---
+++ Experiments/ExperimentBase.cs
@@ -0,0 +1,34 @@
+﻿namespace OWMiniature.Experiments
+{
+    /// <summary>
+    /// Base script for all experiments.
+    /// </summary>
+    public abstract class ExperimentBase
+    {
+        /// <summary>
+        /// Indicates whether this experiment is enabled. <para/>
+        /// 
+        /// Generally used to keep experiments available in code but disabled.
+        /// </summary>
+        public virtual bool IsEnabled => true;
+
+        /// <summary>
+        /// Allows us to define any setups the experiment may need.
+        /// </summary>
+        internal virtual void Enable()
+        {
+        }
+
+        /// <summary>
+        /// Allows us to get rid of anything the experiment was using.
+        /// </summary>
+        internal virtual void Disable()
+        {
+        }
+
+        /// <summary>
+        /// Simple enough, triggers whatever this experiment holds!
+        /// </summary>
+        public abstract void Trigger();
+    }
+}
